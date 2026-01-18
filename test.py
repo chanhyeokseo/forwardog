12dd5fb82fe5f8c7e4b0a3e3f1ea31e78af6a942
mushroom1324 <changhyeok.seo2@gmail.com>
2026-01-19 02:21:12 +0900

    remove test file

--- test.py
+++
@@ -1,9 +0,0 @@
-from datadog import initialize, statsd
-
-options = {
-    'statsd_host': '127.0.0.1',
-    'statsd_port': 8125
-}
-initialize(**options)
-
-statsd.gauge('forwardog.dogstatsd.gauge2', 42, tags=['env:test', 'source:forwardog'])
